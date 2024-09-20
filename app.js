f6b521c52608c2073ba09251452a85cd54f66a8d
Ton Minh Loc <pikeyton@gmail.com>
2024-09-20 11:18:09 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const contactsRouter = require("./app/routes/contact.route");
 
@@ -13,4 +14,11 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 }); 
 
+app.use((req, res, next) =>{
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) =>{
+    return res.status(err.statusCode || 500).json({ message: err.message || "Internal Server Error",});
+});
 module.exports = app;
